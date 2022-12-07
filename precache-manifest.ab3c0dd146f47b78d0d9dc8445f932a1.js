2dd3b47127f16af29471bf44fe400bf97eef4a0e
unknown <ibnualsomal@gmail.com>
2022-12-07 17:17:22 +0600

    Updates

---
+++ precache-manifest.ab3c0dd146f47b78d0d9dc8445f932a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0ba1732ddb3f6c56e82",
+    "url": "/images-library/static/css/main.4d82428b.chunk.css"
+  },
+  {
+    "revision": "d0ba1732ddb3f6c56e82",
+    "url": "/images-library/static/js/main.d0ba1732.chunk.js"
+  },
+  {
+    "revision": "40d04ed985da98f3d921",
+    "url": "/images-library/static/js/1.40d04ed9.chunk.js"
+  },
+  {
+    "revision": "0081158d1f713b4b4ea1",
+    "url": "/images-library/static/js/runtime~main.0081158d.js"
+  },
+  {
+    "revision": "68343a97d5eb0c9f09dcc8b278ace148",
+    "url": "/images-library/static/media/bg.68343a97.mp4"
+  },
+  {
+    "revision": "d0390031fdc5a3366f72a0b12a1664fc",
+    "url": "/images-library/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/photos-gallery/precache-manifest.c6e0550a37e8cf3a33dd2d0ea67f37b8.js"
+  "/images-library/precache-manifest.ab3c0dd146f47b78d0d9dc8445f932a1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/photos-gallery/index.html", {
+workbox.routing.registerNavigationRoute("/images-library/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
